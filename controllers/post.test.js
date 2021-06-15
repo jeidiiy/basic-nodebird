f92f639fe8ca6f82aaac9abe0c29e91727c337e5
Ji-Yong, Jeong <wldyd2356@gmail.com>
2021-06-15 23:31:01 +0900

    feat: addPost 테스트 코드 추가

--- controllers/post.test.js
+++ controllers/post.test.js
@@ -1,6 +1,34 @@
 jest.mock('../models/post');
+jest.mock('../models/hashtag');
 const Post = require('../models/post');
-const { removePost } = require('./post');
+const Hashtag = require('../models/hashtag');
+const { addPost, removePost } = require('./post');
+
+describe('addPost', () => {
+  const req = {
+    body: { content: 'hi', url: 'http://localhost' },
+    user: { id: 1 },
+  };
+  const res = {
+    redirect: jest.fn(),
+  };
+  const next = jest.fn();
+
+  test('게시글을 추가하고 메인 페이지로 리다이렉트', async () => {
+    Post.create.mockReturnValue(
+      Promise.resolve({
+        addHashtags(arr) {
+          return Promise.resolve(true);
+        },
+      }),
+    );
+
+    Hashtag.findOrCreate.mockReturnValue(Promise.resolve({}));
+
+    await addPost(req, res, next);
+    expect(res.redirect).toBeCalledWith('/');
+  });
+});
 
 describe('removePost', () => {
   const req = {
@@ -12,11 +40,13 @@ describe('removePost', () => {
   const next = jest.fn();
 
   test('자신의 게시글을 삭제한 후 200 응답을 보냄', async () => {
-    Post.findOne.mockReturnValue(Promise.resolve({
-      removePost(id) {
-        return Promise.resolve(true);
-      },
-    }));
+    Post.findOne.mockReturnValue(
+      Promise.resolve({
+        removePost(id) {
+          return Promise.resolve(true);
+        },
+      }),
+    );
     await removePost(req, res, next);
     expect(res.sendStatus).toBeCalledWith(200);
   });
